4eab18ae8e96adb4ce0e72c0d28e567ed836e856
Gleider Mackedanz <gleider.ec@gmail.com>
2026-04-20 11:41:49 -0300

    feat(word): update scheduler to respect pre-scheduled words (Unit 7)

--- apps/api/src/word/word.module.ts
+++ apps/api/src/word/word.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { WordService } from './word.service';
 import { WordScheduler } from './word.scheduler';
 
 @Module({
+  imports: [PrismaModule],
   providers: [WordService, WordScheduler],
   exports: [WordService],
 })
